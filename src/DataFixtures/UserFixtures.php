cb828569761a0a015c4e4f15797d0e21c11699d0
Axel D <axel@spliiit.com>
2024-05-28 14:15:29 +0200

    feature/gotenberg : add fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\Subscription;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFirstname('John');
+            $user->setLastname('Doe');
+            $user->setEmail('john.doe' . $i . '@example.com');
+            $user->setPassword('password');
+            $user->setRole('ROLE_USER');
+            $user->setSubscriptionId($manager->getRepository(Subscription::class)->findOneBy(['title' => 'Free']));
+            $user->setCreatedAt(new \DateTimeImmutable());
+            $user->setUpdatedAt(new \DateTimeImmutable());
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
