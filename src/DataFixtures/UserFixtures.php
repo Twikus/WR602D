6542950ff93fa42c90a5650da06095853b61c88b
Axel D <axel@spliiit.com>
2024-05-29 18:31:12 +0200

    feature/gotenberg : update user fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,11 +4,19 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use App\Entity\Subscription;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private  UserPasswordHasherInterface $userPasswordHasher; 
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 10; $i++) {
@@ -16,7 +24,12 @@ class UserFixtures extends Fixture
             $user->setFirstname('John');
             $user->setLastname('Doe');
             $user->setEmail('john.doe' . $i . '@example.com');
-            $user->setPassword('password');
+            $user->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $user,
+                    'password'
+                )
+            );
             $user->setRoles(['ROLE_USER']);
             $user->setSubscriptionId($manager->getRepository(Subscription::class)->findOneBy(['title' => 'Free']));
             $manager->persist($user);
